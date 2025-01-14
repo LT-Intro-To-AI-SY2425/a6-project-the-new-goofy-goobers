16da7cacefb2058b7ed130b7f000363fb4c64fe1
Trevor Jordan <tmjordan5@cps.edu>
2025-01-14 08:44:19 -0600

    pain

--- model.py
+++ model.py
@@ -7,20 +7,24 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 "make da model"
 data = pd.read_csv("data.csv")
-
-
 "RAHHHHHHHHHH"
-# x=data[["x1","x2","x3"]].values    PLACEHOLDER REPLACE WITH ACTUAL DATA HEADERS
-# y=data["y"].values                 SAME HERE
-
-
+x=data[["Displaced","Debtor","Gender","Scholarship holder"]].values
+y_raw=data["Target"].values
+y=[]
+for status in y_raw:
+    if status == "Dropout":
+        y.append(0)
+    elif status == "Enrolled":
+        y.append(0.5)
+    elif status == "Graduate":
+        y.append(1)
 xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=.2)
 xtrain=xtrain.reshape(-1,1)
 model=LinearRegression().fit(xtrain,ytrain)
 coef=np.around(model.coef_,2)
 intercept=round(float(model.intercept_),2)
 r2=round(model.score(x,y),2)
-print(f"equation: y={coef[0]}x1 + {coef[1]}x2 + {coef[2]}x3 + {intercept}")
+print(f"equation: y={coef[0]}x1 + {coef[1]}x2 + {coef[2]}x3 + {coef[3]}x4 + {intercept}")
 print("r^2:",r2)
 "testing"
 predict=model.predict(xtest)
@@ -31,4 +35,4 @@ for index in range(len(xtest)):
     actual=ytest[index]
     predicted_y=predict[index]
     x_coord=xtest[index]
-    print(f"x1: {x_coord[0]} x2: {x_coord[1]} x3: {x_coord[2]} actual: {actual} predicted: {predicted_y}")
+    print(f"x1: {x_coord[0]} x2: {x_coord[1]} x3: {x_coord[2]} x4: {x_coord[3]} actual: {actual} predicted: {predicted_y}")
